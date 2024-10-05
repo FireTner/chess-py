19e9c9fdafd11fcaccc9e7f3abd3e8c1a7dfe57b
FireTner <vladik.cerny.2007@seznam.cz>
2024-10-05 23:24:11 +0200

    fixed the search function

--- search.py
+++ search.py
@@ -7,20 +7,31 @@ def search(board: chess.Board, depth = 0):
     return evaluate.evaluate(board)
   
   best_score = INT_MIN
+
+  for move in board.legal_moves:
+    board.push(move)
+
+    score = -search(board, depth - 1)
+    if score > best_score:
+      best_score = score
+
+    board.pop()
+  
+  return best_score
+
+# Find the best move given a current board position
+def find_best_move(board: chess.Board) -> str:
+  best_score = INT_MIN
   best_move = chess.Move.null
 
   for move in board.legal_moves:
     board.push(move)
 
-    score = -search(depth - 1)
+    score = -search(board, 2)
     if score > best_score:
       best_score = score
       best_move = move
 
     board.pop()
   
-  return best_move, best_score
-
-# Find the best move given a current board position
-def find_best_move(board: chess.Board):
-  search(board, 2)
+  return best_move.uci()
