87c2bc51881fbcbe70d90c2bb79d4131340315f5
FireTner <vladik.cerny.2007@seznam.cz>
2024-10-05 23:08:41 +0200

    added a primitive negamax search

--- search.py
+++ search.py
@@ -1,4 +1,26 @@
 import chess
+import evaluate
+from helper import INT_MIN, INT_MAX
 
+def search(board: chess.Board, depth = 0):
+  if depth == 0 or board.is_game_over:
+    return evaluate.evaluate(board)
+  
+  best_score = INT_MIN
+  best_move = chess.Move.null
+
+  for move in board.legal_moves:
+    board.push(move)
+
+    score = -search(depth - 1)
+    if score > best_score:
+      best_score = score
+      best_move = move
+
+    board.pop()
+  
+  return best_move, best_score
+
+# Find the best move given a current board position
 def find_best_move(board: chess.Board):
-  pass
+  search(board, 2)
