b3142bf9fbc127f304e0f22670a1e89e806611e7
FireTner <vladik.cerny.2007@seznam.cz>
2024-10-05 22:42:13 +0200

    simple uci interface implementation

---
+++ search.py
@@ -0,0 +1,4 @@
+import chess
+
+def find_best_move(board: chess.Board):
+  pass
---
+++ uci.py
@@ -0,0 +1,27 @@
+import chess
+import sys
+from search import find_best_move
+
+board = chess.Board()
+
+def handle_message(message):
+  if message == "uci":
+    pass
+  elif message == "isready":
+    print("readyok")
+  elif "ucinewgame" in message:
+    pass
+  elif "position startpos moves" in message:
+    moves = message.split(" ")[3:]
+    board.clear()
+    board.set_fen(chess.STARTING_BOARD_FEN)
+    for move in moves:
+      board.push_uci(move)
+  elif "position fen" in message:
+    fen = " ".join(message.split(" ")[2:])
+    board.set_fen(fen)
+  elif "go" in message:
+    move = find_best_move(board)
+    print(f"bestmove {move}")
+  elif message == "quit":
+    sys.exit(0)
