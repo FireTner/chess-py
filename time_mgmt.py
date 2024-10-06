42d68aea488e55381568f9edad8f96031e4e5f05
FireTner <vladik.cerny.2007@seznam.cz>
2024-10-06 14:43:47 +0200

    added logging for out of time

--- time_mgmt.py
+++ time_mgmt.py
@@ -1,7 +1,12 @@
 import time
+import logging
 
 def calc_tte(remaining_time: int, time_increment: int) -> int:
   return time.time() + remaining_time / 20 + time_increment / 2
 
 def check_time(time_to_end: int, max_margin: int = 100) -> bool:
-  return time_to_end >= time.time()
+  result = time_to_end >= time.time()
+  if result:
+    time_delta = time_to_end - time.time()
+    logging.debug(f"ran out of time {time_delta}")
+  return result
