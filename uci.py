c63f85043b424d5e9a7f6cca21885219e9c29007
FireTner <vladik.cerny.2007@seznam.cz>
2024-10-06 14:29:47 +0200

    improved argument parsing for uci go

--- uci.py
+++ uci.py
@@ -25,14 +25,29 @@ def handle_message(message):
     fen = " ".join(split_message[2:])
     board.set_fen(fen)
   elif split_message[0] == "go":
-    assert split_message[1] == "wtime"
-    wtime = int(split_message[2])
-    assert split_message[3] == "btime"
-    btime = int(split_message[4])
-    assert split_message[5] == "winc"
-    winc = int(split_message[6])
-    assert split_message[7] == "binc"
-    binc = int(split_message[8])
+    logging.debug("go decoded")
+    arguments = {
+      "wtime": 0,
+      "btime": 0,
+      "winc": 0,
+      "binc": 0,
+    }
+
+    i = 1
+    while i < len(split_message):
+      if split_message[i] in arguments:
+        if i + 1 == len(split_message):
+          break
+        arguments[split_message[i]] = int(split_message[i + 1])
+        i += 1
+
+      i += 1
+    
+    wtime = arguments["wtime"]
+    btime = arguments["btime"]
+    winc = arguments["winc"]
+    binc = arguments["binc"]
+
     move = find_best_move(board, calc_tte(wtime, winc) if board.turn else calc_tte(btime, binc))
     print(f"bestmove {move}")
   elif message == "quit":
