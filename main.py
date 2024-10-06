b7513d2f089ea4a04be444f8d45e5c8048616c59
FireTner <vladik.cerny.2007@seznam.cz>
2024-10-06 14:30:32 +0200

    added primitive logging

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 import uci
+import logging
+
+logging.basicConfig(
+  filename="app.log",
+  encoding="utf-8",
+  filemode="w",
+  format="{asctime} - {levelname} - {message}",
+  style="{",
+  datefmt="%Y-%m-%d %H:%M",
+  level=logging.DEBUG,
+)
 
 def main():
-  while True:
-    message = input()
-    uci.handle_message(message)
+  logging.info("Started")
+  try:
+    while True:
+      message = input()
+      logging.info(f"> {message}")
+      uci.handle_message(message)
+  except Exception as e:
+    logging.error(f"error {repr(e)}; ", exc_info=True,stack_info=True)
+
 
 if __name__ == "__main__":
   main()
--- uci.py
+++ uci.py
@@ -2,26 +2,32 @@ import chess
 import sys
 from search import find_best_move
 from time_mgmt import calc_tte
+import logging
 
 board = chess.Board()
 
 def handle_message(message):
   split_message = message.split(" ")
   if message == "uci":
+    logging.debug("uci decoded")
     print("id name tner-py")
     print("id author firetner")
     print("uciok")
   elif "isready" in message:
+    logging.debug("isready decoded")
     print("readyok")
   elif "ucinewgame" in message:
+    logging.debug("ucinewgame decoded")
     pass
   elif "position startpos moves" in message:
+    logging.debug("position startpos decoded")
     moves = split_message[3:]
     board.clear()
     board.set_fen(chess.STARTING_FEN)
     for move in moves:
       board.push_uci(move)
   elif "position fen" in message:
+    logging.debug("position fen decoded")
     fen = " ".join(split_message[2:])
     board.set_fen(fen)
   elif split_message[0] == "go":
@@ -51,4 +57,7 @@ def handle_message(message):
     move = find_best_move(board, calc_tte(wtime, winc) if board.turn else calc_tte(btime, binc))
     print(f"bestmove {move}")
   elif message == "quit":
+    logging.debug("quit decoded")
     sys.exit(0)
+  else:
+    logging.debug("unknown message")
