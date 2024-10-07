c098d2c30cfab5532c39fe95439c440214a2d072
FireTner <vladik.cerny.2007@seznam.cz>
2024-10-07 10:09:16 +0200

    added logging for nodes sesarched

--- search.py
+++ search.py
@@ -1,9 +1,14 @@
 import chess
+import logging
 import evaluate
 from helper import INT_MIN, INT_MAX
 from time_mgmt import check_time
 
+nodes_searched = 0
+
 def search(board: chess.Board, depth: int = 0, time_to_end: int = INT_MAX):
+  global nodes_searched
+  nodes_searched += 1
   if depth == 0 or board.is_game_over():
     return evaluate.evaluate(board)
   
@@ -26,10 +31,13 @@ def search(board: chess.Board, depth: int = 0, time_to_end: int = INT_MAX):
 
 # Find the best move given a current board position
 def find_best_move(board: chess.Board, time_to_end: int) -> str:
+  global nodes_searched
+  nodes_searched = 0
   best_score = INT_MIN
   best_move = chess.Move.null
 
   for move in board.legal_moves:
+    nodes_searched += 1
     board.push(move)
 
     score = -search(board, 2, time_to_end)
@@ -43,4 +51,6 @@ def find_best_move(board: chess.Board, time_to_end: int) -> str:
 
     board.pop()
   
+  logging.info(f"searched {nodes_searched} nodes")  
+
   return best_move.uci()
