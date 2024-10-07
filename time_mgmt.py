a3e1dadb2dfeaea8e7890b386fa4db85ea871c33
FireTner <vladik.cerny.2007@seznam.cz>
2024-10-07 11:37:56 +0200

    added debug for time mgmt

--- time_mgmt.py
+++ time_mgmt.py
@@ -5,4 +5,7 @@ def calc_tte(remaining_time: int, time_increment: int) -> int:
   return time.time() + (remaining_time / 20 + time_increment / 2) / 1000
 
 def check_time(time_to_end: int, max_margin: int = 100) -> bool:
-  return time_to_end <= time.time()
+  result = time_to_end <= time.time()
+  if result:
+    logging.debug("ran out of time")
+  return result
